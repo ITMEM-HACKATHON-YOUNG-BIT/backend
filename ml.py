50d35b5fceadf9d7baff4074c348d71b63d637d0
Trofimov Maksim <maks.trofimov.0303@mail.ru>
2023-02-19 08:52:27 +0300

    add code

--- ml.py
+++ ml.py
@@ -1,5 +1,7 @@
 from typing import List, Dict
 import datetime
+
+import db
 from texts import *
 import json
 import time
@@ -8,9 +10,12 @@ from sklearn.metrics.pairwise import cosine_similarity
 from transformers import AutoTokenizer, AutoModel
 import torch
 import numpy as np
+import pandas as pd
+import sklearn
+# from sklearn import cross_validation as cv
 
 
-tokenizer = AutoTokenizer.from_pretrained('cointegrated/rubert-tiny')  # sberbank-ai/sbert_large_nlu_ru
+tokenizer = AutoTokenizer.from_pretrained('cointegrated/rubert-tiny')  # or sberbank-ai/sbert_large_nlu_ru
 model = AutoModel.from_pretrained('cointegrated/rubert-tiny')
 
 
@@ -69,6 +74,43 @@ def get_age(birthday: str):
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
 
+def predict(ratings, similarity, type='user'):
+    if type == 'user':
+        mean_user_rating = ratings.mean(axis=1)
+        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif type == 'item':
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+    return pred
+
+
+def pretrain():
+    df = pd.read_csv('ml-latest-small/ratings.csv')
+    n_users = df['userId'].unique().shape[0]
+    n_items = df['eventId'].unique().shape[0]
+    input_list = df['eventId'].unique()
+
+    def scale_movie_id(input_id):
+        return np.where(input_list == input_id)[0][0] + 1
+
+    df['movieId'] = df['movieId'].apply(scale_movie_id)
+
+    train_data, test_data = sklearn.model_selection.train_test_split(df, train_size=.8)
+    train_data_matrix = np.zeros((n_users, n_items))
+    for line in train_data.itertuples():
+        train_data_matrix[line[1] - 1, line[2] - 1] = line[3]
+
+    test_data_matrix = np.zeros((n_users, n_items))
+    for line in test_data.itertuples():
+        test_data_matrix[line[1] - 1, line[2] - 1] = line[3]
+
+    user_similarity = sklearn.metrics.pairwise.pairwise_distances(train_data_matrix, metric='cosine')
+    item_similarity = sklearn.metrics.pairwise.pairwise_distances(train_data_matrix.T, metric='cosine')
+
+    # item_prediction = predict(train_data_matrix, item_similarity, type='item')
+    # user_prediction = predict(train_data_matrix, user_similarity, type='user')
+
+
 def match(user: dict, event: dict) -> bool:
     if not eq_regions(user['region'], event['region']):
         return False
@@ -76,7 +118,7 @@ def match(user: dict, event: dict) -> bool:
     if not (event['ageFrom'] <= get_age(user['birthday']) <= event['ageTo']):
         return False
 
-    # TODO: add ml functions
+    predict(db.Events.select(), db.Users.select())
     return True
 
 
@@ -93,6 +135,8 @@ def classification_users_to_events(users: List[Dict], events: List[Dict]):
                                                          event['registrationEndsAt'],
                                                          event['beginsAt'],
                                                          event['endsAt'],
+                                                         event['minAmount'],
+                                                         event['maxAmount'],
                                                          event['url'])
     for user in users:
         for index, event in enumerate(events):
